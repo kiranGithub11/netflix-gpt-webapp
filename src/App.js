86be7ca7beac6322663c787caa328bec6ad780af
@kiranGithub11 <kirannaragam@gmail.com>
2024-12-18 10:12:42 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center font-bold text-5xl text-green-500 p-5 bg-slate-300">
+      <h1>Kiran Naragam</h1>
+    </div>
+  );
+}
+
+export default App;
